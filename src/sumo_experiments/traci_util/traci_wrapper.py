9c9e34e54dfc4fc0a3c415687573d29d42100759
Jules Bompard <jules.bompard@outlook.fr>
2023-10-25 09:43:38 +0200

    Add documentation to traci_wrapper.py

--- src/sumo_experiments/traci_util/traci_wrapper.py
+++ src/sumo_experiments/traci_util/traci_wrapper.py
@@ -5,6 +5,15 @@ import traci
 class TraciWrapper:
     """
     Wrap TraCi functions to make only one
+    The run_traci method from the Experiment class can only take one traci function as argument.
+    The aim of the traci wrapper is to group multiple functions in only one object (and one final_function) to be passed to run_traci.
+    Two types of traci functions are defined here :
+    - Stats functions are functions that look at the simulation and returns statistics about it. This functions must return a dict object
+    with the name of the data as key, and the data as value. The data can't be an iterable object.
+    - Behavioural functions modify the behaviour of infrastructures within the simulation. This functions must return a new config that will be used
+    in the next iterations.
+    All of this function must accept a config as argument.
+    The order of the functions is important : think about it when you use this wrapper.
     """
 
     def __init__(self):
@@ -13,7 +22,6 @@ class TraciWrapper:
         """
         self.stats_functions = []
         self.behavioural_functions = []
-        self.config = {}
         self.data = {'simulation_step': []}
 
     def add_stats_function(self, function):
@@ -45,8 +53,8 @@ class TraciWrapper:
         :return: dict
         """
         self.data = {'simulation_step': []}
-        self.config = config
-        simulation_duration = self.config['simulation_duration']
+        current_config = config
+        simulation_duration = current_config['simulation_duration']
         step = 0
 
         while step < simulation_duration:
@@ -55,7 +63,7 @@ class TraciWrapper:
 
             # Statistical functions
             for stats_function in self.stats_functions:
-                res = stats_function(self.config)
+                res = stats_function(current_config)
                 for key in res:
                     if key in self.data:
                         self.data[key].append(res[key])
@@ -64,8 +72,8 @@ class TraciWrapper:
 
             # Behavioural functions
             for bahavioural_function in self.behavioural_functions:
-                res = bahavioural_function(self.config)
-                self.config = res
+                res = bahavioural_function(current_config)
+                current_config = res
 
             self.data['simulation_step'].append(step + 1)
             step += 1
