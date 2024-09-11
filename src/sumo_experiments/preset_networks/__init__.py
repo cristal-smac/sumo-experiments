e644a301e577b94ade149c6df2440179238a3a76
Jules Bompard <jules.bompard@outlook.fr>
2024-09-11 15:47:01 +0200

    Corrected dependency error in pyproject.toml Removed LilleNetwork import from init in preset_networks

--- src/sumo_experiments/preset_networks/__init__.py
+++ src/sumo_experiments/preset_networks/__init__.py
@@ -1,5 +1,4 @@
 from .one_crossroad_network import OneCrossroadNetwork
 from .line_network import LineNetwork
 from .grid_network import GridNetwork
-from .lille_network import LilleNetwork
 from .bologna_network import BolognaNetwork
