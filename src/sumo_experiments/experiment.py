13247264ed16df06e049ca9e65e220b0d81b02b7
Damian <damian.dailisan@gmail.com>
2025-10-10 11:34:20 +0200

    revert teleport time to 150 by default

--- src/sumo_experiments/experiment.py
+++ src/sumo_experiments/experiment.py
@@ -122,7 +122,7 @@ class Experiment:
             else:
                 traci.start(["sumo"] + args.split())
         else:
-            traci.start((self.full_line_command + ' --time-to-teleport -1 --no-warnings').split())
+            traci.start((self.full_line_command + ' --time-to-teleport 150 --no-warnings').split())
 
         res = traci_function(traci)
 
